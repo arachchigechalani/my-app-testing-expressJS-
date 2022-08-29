447ac8d0120541b93010b38cc0e02576119c06fa
arachchigechalani <arachchigechalani@gmail.com>
2022-08-30 02:05:34 +0530

    updated

--- app.js
+++ app.js
@@ -6,18 +6,25 @@ app.use(express.json())
 
 app.get('/', (req, res) => {
   console.log(`get request has come`)
-  res.send('<h1>Hello World !<h1/>')
+  res.send('<h1>Hello World get!<h1/>')
 })
-app.get('/customer', (req, res) => {
-    console.log(req.body)
-    res.send('Hello customer!')
+
+app.post('/customer', (req, res) => {
+    console.log(req.body.name);
+    res.send('Hello customer post!')
   })
 
-app.get('/customer/:id', (req, res) => {
+  //params wlin ena ekk alla ganima
+app.delete('/customer/:id', (req, res) => {
     console.log(req.params)
-    res.send('Hello customer id!')
+    res.send('Hello customer id using params!')
+  })
+  
+  //Query parameter wlin ena ekk allaganima
+  app.delete('/customer', (req, res) => {
+    console.log(req.query.id)
+    res.send('delete id using query parameter!')
   })  
-
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
